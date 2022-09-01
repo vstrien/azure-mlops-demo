81ed11ff9ced1107e6349a4f8e2a0c32f471e3b7
Koos van Strien <vstrien@gmail.com>
2022-09-01 11:14:47 +0200

    Update create-components.ps1
    The -f parameter requires a path to the yaml file (it pointed to a folder which gives an error with `az ml component create -f`)

--- create-components.ps1
+++ create-components.ps1
@@ -2,8 +2,8 @@ $ResourceGroup = $env:AZURE_ML_RESOURCE_GROUP
 $WorkspaceName = $env:AZURE_ML_WORKSPACE
 
 $components = @(
-    "./components/fix_missing_values"
-    "./components/select_features"
+    "./components/fix_missing_values/fix-missing-values.yml"
+    "./components/select_features/select-features.yml"
 )
 
 foreach($component in $components) {
